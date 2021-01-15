be4834ac90cbb1d6ff5215f3d1e5b339b1de5388
subinda9 <subinda9@gmail.com>
2021-01-15 19:38:39 +0530

    web code

---
+++ build/precache-manifest.d87ed56bed7ed1fa34addc46555915ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856a05abc1b11fda0541a334ef672a44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20b7b1eede5b7af0ff06",
+    "url": "/static/css/main.13954578.chunk.css"
+  },
+  {
+    "revision": "dc7246cccc5275efdfa7",
+    "url": "/static/js/2.31790f4f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.31790f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b7b1eede5b7af0ff06",
+    "url": "/static/js/main.12a84375.chunk.js"
+  },
+  {
+    "revision": "15f26ed32195f4fefa40",
+    "url": "/static/js/runtime-main.bb5b0099.js"
+  },
+  {
+    "revision": "969662303a44a89c88de09af8873c6c8",
+    "url": "/static/media/bgp.96966230.jpg"
+  }
+]);
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+
+    return (
+            <div className="nav">
+                <h1 className="title">Weather App @subinda</h1>
+            </div>
+    )
+}
+
